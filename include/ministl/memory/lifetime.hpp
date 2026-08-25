717a32c9681ab29ccb56219812a74cb5133911d8
Indranil Mukherjee <imukherjee@microsoft.com>
2026-08-25 17:33:54 +0530

    memory: add object lifetime utilities

---
+++ include/ministl/memory/lifetime.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <new>
+#include <utility>
+#include <memory>
+
+namespace ministl {
+    template<typename T, typename... Args>
+    constexpr T *construct_at(T *ptr, Args &&... args) {
+        return ::new(static_cast<void *>(ptr))T(std::forward<Args>(args)...);
+    }
+
+    template<typename T>
+    constexpr void destroy_at(T *ptr) {
+        ptr->~T();
+    }
+
+    template<typename ForwardIt>
+    constexpr void destroy_range(ForwardIt first,
+                                 ForwardIt last) {
+        while (first != last) {
+            destroy_at(std::addressof(*first));
+            ++first;
+        }
+    }
+}
